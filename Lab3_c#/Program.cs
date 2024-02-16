c7c99ae5d0b1b05afb0ade80aa30f31c98d4d164
l-Mel-l <125726970+l-Mel-l@users.noreply.github.com>
2024-02-17 01:48:14 +0300

    Добавьте файлы проекта.

---
+++ Lab3_c#/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Таблица_Лаб3
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine(i + "*" + j + "=" + i * j);
+            }
+            Console.WriteLine("---------");
+        }
+    }
+}
